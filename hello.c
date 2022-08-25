ac448356b6951ead5a5335f60ec3a5eec85346a9
Your Name <youremail@huskers.unl.edu>
2022-08-25 20:05:27 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+ * Authors: Benny Ho
+ * Date: 2022/08/24
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char **argv) {
+
+    printf("Hi Benny!\n");
+
+    return 0;
+}
